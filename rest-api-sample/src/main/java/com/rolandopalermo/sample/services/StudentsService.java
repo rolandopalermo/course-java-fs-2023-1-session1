af2cb76d3e152d97ff8459f6cd48d4de6f6d9cac
rolandopalermo <support@rolandopalermo.com>
2023-06-05 13:07:46 -0600

    Add changes

--- rest-api-sample/src/main/java/com/rolandopalermo/sample/services/StudentsService.java
+++ rest-api-sample/src/main/java/com/rolandopalermo/sample/services/StudentsService.java
@@ -6,9 +6,16 @@ import com.rolandopalermo.sample.repositories.StudentsRepository;
 import com.rolandopalermo.sample.repositories.entities.Student;
 import com.rolandopalermo.sample.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.text.MessageFormat.format;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +24,50 @@ public class StudentsService {
     private final StudentsRepository studentsRepository;
 
     public StudentCreationResponseDto create(StudentCreationRequestDto request) {
+        Optional<Student> existingStudent = studentsRepository.findByIdentificationNumber(request.getIdNumber());
+
+        if (existingStudent.isPresent()) {
+            throw new RuntimeException(format("A student with id number {0} already exists", request.getIdNumber()));
+        }
+
         Student createdStudent = studentsRepository.save(toEntity(request));
         return toDto(createdStudent);
+    }
+
+    public StudentCreationResponseDto findById(long id) {
+        Student student = findFirstById(id);
+        return toDto(student);
+    }
+
+    public StudentCreationResponseDto update(long id, StudentCreationRequestDto request) {
+        Student student = findFirstById(id);
+        student.setFirstName(request.getFirstName());
+        student.setLastName(request.getLastName());
+        student.setBirthday(DateUtils.toLocalDate(request.getBirthday()));
+
+        Student updatedStudent = studentsRepository.save(student);
+        return toDto(updatedStudent);
+    }
+
+    public Page<StudentCreationResponseDto> findAll(Pageable pageable) {
+        Page<Student> studentsPage = studentsRepository.findAll(pageable);
+
+        return new PageImpl<>(
+                studentsPage.getContent()
+                        .stream()
+                        .map(this::toDto)
+                        .collect(Collectors.toList()),
+                studentsPage.getPageable(),
+                studentsPage.getTotalElements());
+    }
+
+    public void delete(long id) {
+        studentsRepository.findById(id).ifPresent(studentsRepository::delete);
+    }
+
+    private Student findFirstById(long id) {
+        return studentsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(format("Unable to find a student with id {0}", id)));
     }
 
     private Student toEntity(StudentCreationRequestDto request) {
@@ -27,6 +76,7 @@ public class StudentsService {
         student.setLastName(request.getLastName());
         student.setBirthday(DateUtils.toLocalDate(request.getBirthday()));
         student.setInsertionDate(LocalDateTime.now());
+        student.setIdentificationNumber(request.getIdNumber());
         return student;
     }
 
@@ -36,8 +86,8 @@ public class StudentsService {
         student.setFirstName(entity.getFirstName());
         student.setLastName(entity.getLastName());
         student.setBirthday(DateUtils.toString(entity.getBirthday()));
+        student.setIdNumber(entity.getIdentificationNumber());
         return student;
     }
-
 
 }
