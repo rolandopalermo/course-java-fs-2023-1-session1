0c5d76b8ed67b9fa56be877084e9dcbf996d05c6
rolandopalermo <rolando.rodriguez@rappi.com>
2023-06-19 18:21:08 -0600

    Undo test changes

--- app-invoicing/src/main/java/com/rpconsulting/app/invoicing/services/InvoicesService.java
+++ app-invoicing/src/main/java/com/rpconsulting/app/invoicing/services/InvoicesService.java
@@ -4,11 +4,8 @@ import com.rpconsulting.app.invoicing.dtos.invoices.InvoiceCreationRequestDto;
 import com.rpconsulting.app.invoicing.dtos.invoices.InvoiceCreationResponseDto;
 import com.rpconsulting.app.invoicing.dtos.invoices.InvoiceDetailSummaryDto;
 import com.rpconsulting.app.invoicing.dtos.invoices.InvoiceResponseDto;
-import com.rpconsulting.app.invoicing.repositories.projections.InvoiceDetailProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface InvoicesService {
 
